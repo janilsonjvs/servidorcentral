4f3baf3aa028459f70939cec7853b8972df7930f
janilsonjvs <janilson.varela@escola.ifrn.edu.br>
2015-07-12 13:39:17 -0300

    ServidorCentral
    Ajustes

--- NovoServidor.rb
+++
@@ -1,8 +0,0 @@
-require 'socket'
-server = TCPServer.new(2010)
-puts "Servidor Iniciado"
-puts "Lista de Servidor(es) registrado(s)"
-
-f = File.new("ListaServers.txt","a+")
-
-f.each {|line| print line}
--- servidorcentral.rb
+++ servidorcentral.rb
@@ -1,1 +1,8 @@
 require 'socket'
+server = TCPServer.new(2010)
+puts "Servidor Iniciado"
+puts "Lista de Servidor(es) registrado(s)"
+
+f = File.new("ListaServers.txt","a+")
+
+f.each {|line| print line}
