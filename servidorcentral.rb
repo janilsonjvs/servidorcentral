e5bcc145cde013bb548ee0e8d008a0da1337a3d1
janilsonjvs <janilson.varela@escola.ifrn.edu.br>
2015-07-12 20:07:43 -0300

    Revisao

--- servidorcentral.rb
+++ servidorcentral.rb
@@ -6,33 +6,16 @@ server.bind("",2010)
 
 puts "Servidor Iniciado"
 puts "Lista de Servidor(es) registrado(s)"
-
-f = File.new("ListaServers.txt","w+")
-bip = false
-
+f = File.new("ListaServers.txt")
 f.each {|line| print line}
 f.close
 loop{
 	data, sender = server.recvfrom(1024)
-	c_ip = sender[3] # ip do cliente
-	c_porta = sender[1] # porta do cliente
-	puts "IP do cliente Conectado: #{c_ip}"
-	f2 = File.new("ListaServers.txt")
-	f2.each {|li| 
-		puts c_ip
-		if (li == c_ip) then
-			bip = true
-		end
-	}
-	f2.close
+	arr = data.split
 
-	if (bip == false) then
-		f3 = File.new("ListaServers.txt","a+")
-		f3.puts c_ip
-		f3.close	
-		puts "Novo IP registrado: #{c_ip}"
+	if arr[0] == 'REG'
+		puts "Solicitacao de REG"
+		puts "Dominio para registro: "+arr[1]
+		puts "IP do Dominio: "+arr[2]
 	end
 }
-
-
-
