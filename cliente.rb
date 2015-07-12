04a07660dcc0079825a711e0d6c7a0d23872cd23
janilsonjvs <janilson.varela@escola.ifrn.edu.br>
2015-07-12 18:39:00 -0300

    Updates

--- cliente.rb
+++ cliente.rb
@@ -1,1 +1,9 @@
 require 'socket'
+socket = UDPSocket.new
+socket.connect('localhost',2010)
+loop{
+msg = gets
+socket.puts msg
+puts socket.gets
+}
+socket.close
--- servidorcentral.rb
+++ servidorcentral.rb
@@ -1,21 +1,38 @@
 require 'socket'
 
 
-server = TCPServer.new(2010)
+server = UDPSocket.new
+server.bind("",2010)
+
 puts "Servidor Iniciado"
 puts "Lista de Servidor(es) registrado(s)"
 
-f = File.new("ListaServers.txt")
+f = File.new("ListaServers.txt","w+")
+bip = false
 
 f.each {|line| print line}
 f.close
-client = server.accept
 loop{
 	data, sender = server.recvfrom(1024)
 	c_ip = sender[3] # ip do cliente
 	c_porta = sender[1] # porta do cliente
-	puts "Dados do cliente #{c_ip}"
+	puts "IP do cliente Conectado: #{c_ip}"
+	f2 = File.new("ListaServers.txt")
+	f2.each {|li| 
+		puts c_ip
+		if (li == c_ip) then
+			bip = true
+		end
+	}
+	f2.close
 
+	if (bip == false) then
+		f3 = File.new("ListaServers.txt","a+")
+		f3.puts c_ip
+		f3.close	
+		puts "Novo IP registrado: #{c_ip}"
+	end
 }
+
 
 
