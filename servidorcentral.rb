36d203a858933ef02ef6c79c9b0f417a285507c3
janilsonjvs <janilson.varela@escola.ifrn.edu.br>
2015-07-13 23:33:13 -0300

    Ajuste banco de dados
    Adicionado codigo para acesso a banco de dados

--- servidorcentral.rb
+++ servidorcentral.rb
@@ -2,72 +2,59 @@
 #Data: 12/07/2015
 #Servidor para registro de dominio
 
-require 'socket'
-server = TCPServer.new(2100)
 
-puts "Servidor de Registro iniciado."
-puts "Lista de Servidor(es) registrado(s)"
-f = File.new("ListaServers.txt")
-f.each {|line| print line}
-f.close
+require 'socket'
+require 'rdbi-driver-sqlite3'
+
+server = UDPSocket.new
+server.bind("", 2100)
+puts "Servidor Central iniciado"
+dbh = RDBI.connect( :SQLite3, :database => "dbregistro.db" )
+aspas = '"'
 msgREGOK = "REGOK"
 msgREGFALHA = "REGFALHA"
+msgIPOK = "IPOK"
 msgIPFALHA = "IPFALHA"
 msgFALHA = "FALHA"
+res = nil
+	#Lista os Dominios e IP registrados
+	rs = dbh.execute("select IP from dns")
+	rs.fetch(:all).each do |row|
+		puts row[0]
+	end
 
-loop{
-	clientSocket =  server.accept
-	data = clientSocket.gets
-	arr = data.split
-	rge = false
-
-	if arr[0] == 'REG' then
-		puts "Solicitacao de REG"
-
-		f1 = File.new("ListaServers.txt")
-		f1.each {|li|
-			dom = li.split
-
-			if dom[1] == arr[1]
-				puts "Dominio ja registrado!"	
-				clientSocket.puts(msgREGFALHA) 
-				rge = true
-				break
+loop {
+	dados, sender = server.recvfrom(1024)
+	
+	arr = dados.split
+	c_ip = sender[3]
+	c_port = sender[1]
+	if (arr[0] == "IP" and arr.length == 2) then
+		if (arr[1] != nil) then
+			rs = dbh.execute("select ip from dns where dominio = #{aspas} #{arr[1]} #{aspas}")
+			rs.fetch(:all).each do |row|
+				res = row[0]
 			end
-		}
-		f1.close
+			puts res
+			if res != nil then
+				server.send( msgIPOK + "#{res}", 0, c_ip, c_porta)
+			elsif (res == nil) then
+				server.send  msgIPFALHA, 0, c_ip, c_porta
+			end
 
-		if rge == false
-			f1 = File.new("ListaServers.txt","a+")
-			f1.puts data
-			f1.close
-			puts "Dominio para registro: "+arr[1]
-			puts "IP do Dominio: "+arr[2]	
-			clientSocket.puts(msgREGOK) 
 		end
-
-
-	elsif arr[0] == 'IP' then
-		puts "Solicitacao de IP"
-
-		f1 = File.new("ListaServers.txt")
-		f1.each {|li|
-			dom = li.split
-
-			if dom[1] == arr[1]
-				rge = true
-				puts dom[2]
-				clientSocket.puts("IPOK "+dom[2].to_s) 
-				break
+	elsif (arr[0] == "REG" and arr.length == 3) then
+		if (arr[1] != nil and arr[2] != nil) then
+			begin
+				dbh.execute("insert into dns (dominio, ip) values ( #{aspas}#{arr[1]}#{aspas}, #{aspas}#{arr[2]}#{aspas})")
+				server.send msgREGOK, 0 , c_ip, c_porta
+			rescue
+				server.send msgREGFALHA, 0, c_ip, c_porta
 			end
-		}
-		f1.close		
 
-		if rge == false
-			clientSocket.puts(msgIPFALHA) 
 		end
 	else
-		clientSocket.puts(msgFALHA) 
-		
+		server.send msgFALHA, 0, c_ip, c_porta	
 	end
 }
+server.close
